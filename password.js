558072198af5e4ee93665cf7d5735a08a3299b7c
kevin pena <kevinpena421@gmail.com>
2022-08-24 11:02:58 -0500

    added conditions and finalized project

--- password.js
+++ password.js
@@ -1,35 +1,50 @@
-let char = "abcdefghijklmnopqrstuvwxyz1234567890~”#$%&`()*+-./:;<=>?@[\^`{|}~"
+let letters = 'abcdefghijklmnopqrstuvwxyz' 
+let numbers = '1234567890' 
+let specialCharacters = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
-let gen = document.getElementById("generate")
-    .addEventListener('click', passgen );
+let button = document.getElementById("generate")
+    .addEventListener('click', passgen);
 
-function passgen() {
-    confirm("Do you want to create a new password?");
-    let length = prompt("How long do you want it to be 8-128"); 
-    val = "";
-    
-    for (let a = 0; a < length; ++a) {
-        val += char.charAt(Math.floor(Math.random() * char.length));
-        return val;
-        
+    function passgen() {
+        pass = '';
+        val = '';
+        confirm("Do you want to create a new password?");
+        let length = prompt("How long do you want it to be 8-128");
+        let lowercase = confirm("Do you want to have lowercase letters?");
+        let uppercase = confirm("Do you want to have uppercase letters?");
+        let numeric =   confirm("Do you want to include numbers?");
+        let special =   confirm("Do you want to include special characters?");
+
+
+    if (lowercase) {
+        val += letters.toLowerCase();
     };
-};
 
-let case1 = document.getElementById("generate")
-    .addEventListener('click', lowerCase );
+    if (uppercase) {
+        val += letters.toUpperCase();
+    };
 
+    if (numeric) {
+        val += numbers;
+    };
 
-function lowerCase () {
-    confirm("Do you want to include lowercase?");
-        let lower = "";
-        
-            if (confirm === confirm) {
-                lower = char.toLowerCase;
+    if (special) {
+        val += specialCharacters;        
+    };
+    
             
-            };
+
+    for (let a = 0; a < length; ++a) {
+        pass += val [Math.floor(Math.random() * val.length)];
+            
+    };
+    
+        document.getElementById("password").innerHTML = pass;
 };
 
 
+
+    
     
     
 
